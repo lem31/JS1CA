9bd434d347961e95098b03ca4aa426c62bb1ccca
Leanne <lemeyer.no@gmail.com>
2024-03-02 18:53:19 +0100

    updated

--- JS FILES/checkout-success.js
+++ JS FILES/checkout-success.js
@@ -18,9 +18,17 @@ const orderDetails = document.getElementById('checkout-text');
 const orderDetailsBox = document.createElement('p');
 orderDetailsBox.classList.add('order-number-info')
 
+const orderDetailsText1 = document.createElement('p');
+orderDetailsText1.classList.add('order-details-one');
+orderDetailsText1.textContent = 'Thank you for your purchase.'
+
+const orderDetailsText2 = document.createElement('p');
+orderDetailsText2.classList.add('order-details-two');
+orderDetailsText2.textContent = 'You will receive a confirmation email, shortly.'
+
 orderDetailsBox.textContent = 'Your order number is:'+'' + orderNumber; 
 
-orderDetails.append(orderDetailsBox);
+orderDetails.append(orderDetailsText1, orderDetailsBox, orderDetailsText2);
 
 console.log(orderDetailsBox);};
 
